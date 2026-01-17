5aaeea620382dbb8c681218912519ec66399ec32
boonewr <wrboone12@gmail.com>
2026-01-17 14:55:54 -0500

    Added click counter to server

--- main.go
+++ main.go
@@ -6,21 +6,33 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"sync"
+)
+
+var (
+	mu    sync.Mutex
+	count int
 )
 
 func main() {
+	// Serve React static files from the "dist" directory
 	fs := http.FileServer(http.Dir("./dist"))
 
 	// API Handler
 	http.HandleFunc("/api/ping", func(w http.ResponseWriter, r *http.Request) {
+		mu.Lock()
+		count++
+		current := count
+		mu.Unlock()
+
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(map[string]string{
-			"message": "let's go",
-			"time":    "Server active",
+		json.NewEncoder(w).Encode(map[string]any{
+			"message": "Let's Go",
+			"count":   current,
 		})
 	})
 
-	// serve index.html
+	// serve index.html 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		path := filepath.Join("dist", r.URL.Path)
 		if _, err := os.Stat(path); os.IsNotExist(err) {
